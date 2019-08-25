0ad4ab7c28bb693df35c7030649796fb398ebe65
Emine Hilal Ersoy <hilal@Emine-MacBook-Air.local>
2019-08-25 04:31:04 +0300

    Grid system implemented

---
+++ Assets/Scripts/Runtime/ObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Hexic.Elements;
+
+namespace Hexic.Runtime
+{
+
+public class ObjectPool : MonoBehaviour
+{
+    public List<Cell> cellPool = new List<Cell>();
+
+        public Cell ReuseCell()
+        {
+            var _cell= cellPool[0];
+            _cell.OnReuse();
+            cellPool.RemoveAt(0);
+            cellPool.Add(_cell);
+            return _cell;
+        }
+    }
+}
